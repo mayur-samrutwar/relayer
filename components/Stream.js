385dce125f83922f86d0cc48ef74348e7aaebc1c
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2024-11-15 23:08:46 +0700

    feat: animated edge

--- components/Stream.js
+++ components/Stream.js
@@ -1,3 +1,11 @@
 export default function Stream() {
-  return <div>Stream</div>;
+  return (
+    <div className="flex items-center justify-center p-8">
+      <div className="flex items-center">
+        <div className="border p-4 rounded shadow z-10 bg-white">Card 1</div>
+        <div className="w-24 h-[2px] -mx-2 animate-move-dots"></div>
+        <div className="border p-4 rounded shadow z-10 bg-white">Card 2</div>
+      </div>
+    </div>
+  );
 }
