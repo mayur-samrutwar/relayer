151da49a6777a9aec1f0090d7414f42dadde45e0
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2024-11-17 01:55:49 +0700

    add: db connect file

--- components/Stream.js
+++ components/Stream.js
@@ -2,7 +2,7 @@ export default function Stream() {
   return (
     <div className="flex flex-col items-center p-8">
       {/* Top card */}
-      <div className="border p-4 rounded shadow z-10 bg-white">Top Card</div>
+      <div className="border p-4 rounded shadow z-10 bg-white">This NFT</div>
       
       {/* Vertical line from top card */}
       <div className="h-16 w-[2px] animate-move-dots-vertical"></div>
@@ -14,14 +14,14 @@ export default function Stream() {
         <div className="flex flex-col items-center">
           <div className="w-[8rem] h-[2px] translate-x-16 animate-move-dots transform rotate-180"></div>
           <div className="h-16 w-[2px] animate-move-dots-vertical "></div>
-          <div className="border p-4 rounded shadow z-10 bg-white">Card 1</div>
+          <div className="border p-4 rounded shadow z-10 bg-white">Parent 1</div>
         </div>
 
         {/* Right path */}
         <div className="flex flex-col items-center">
           <div className="w-[8rem] h-[2px] -translate-x-16 animate-move-dots"></div>
           <div className="h-16 w-[2px] animate-move-dots-vertical"></div>
-          <div className="border p-4 rounded shadow z-10 bg-white">Card 2</div>
+          <div className="border p-4 rounded shadow z-10 bg-white">Parent 2</div>
         </div>
       </div>
     </div>
