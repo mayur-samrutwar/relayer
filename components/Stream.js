fe3cf325cfd3f1aca02a7eba28760f1f4b151cda
Mayur Samrutwar <samrutwarmayur1@gmail.com>
2024-11-16 21:59:32 +0700

    update: simple contract

--- components/Stream.js
+++ components/Stream.js
@@ -1,31 +1,29 @@
 export default function Stream() {
   return (
     <div className="flex flex-col items-center p-8">
-      {/* Top cards container */}
-      <div className="flex justify-center gap-32 w-full relative">
-        {/* Left card */}
-        <div className="flex flex-col items-center">
-          <div className="border p-4 rounded shadow z-10 bg-white">Card 1</div>
-          <div className="h-16 w-[2px] animate-move-dots-vertical"></div>
-          <div className="w-[8rem] h-[2px] translate-x-16 animate-move-dots"></div>
-        </div>
-
-        {/* Right card */}
-        <div className="flex flex-col items-center">
-          <div className="border p-4 rounded shadow z-10 bg-white">Card 2</div>
-          <div className="h-16 w-[2px] animate-move-dots-vertical"></div>
-          <div className="w-[8rem] h-[2px] -translate-x-16 animate-move-dots transform rotate-180"></div>
-        </div>
-      </div>
+      {/* Top card */}
+      <div className="border p-4 rounded shadow z-10 bg-white">Top Card</div>
       
-      {/* Connection point */}
-      <div className="absolute left-1/2 w-2 h-2 bg-white border rounded-full -translate-x-1/2 z-20"></div>
-      
-      {/* Vertical line to bottom card */}
+      {/* Vertical line from top card */}
       <div className="h-16 w-[2px] animate-move-dots-vertical"></div>
       
-      {/* Bottom card */}
-      <div className="border p-4 rounded shadow z-10 bg-white">Bottom Card</div>
+      {/* Horizontal split lines container */}
+      <div className="flex justify-center gap-32 w-full relative">
+   
+        {/* Left path */}
+        <div className="flex flex-col items-center">
+          <div className="w-[8rem] h-[2px] translate-x-16 animate-move-dots transform rotate-180"></div>
+          <div className="h-16 w-[2px] animate-move-dots-vertical "></div>
+          <div className="border p-4 rounded shadow z-10 bg-white">Card 1</div>
+        </div>
+
+        {/* Right path */}
+        <div className="flex flex-col items-center">
+          <div className="w-[8rem] h-[2px] -translate-x-16 animate-move-dots"></div>
+          <div className="h-16 w-[2px] animate-move-dots-vertical"></div>
+          <div className="border p-4 rounded shadow z-10 bg-white">Card 2</div>
+        </div>
+      </div>
     </div>
   );
 }
--- contract/relayer.sol
+++ contract/relayer.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTShares is ERC721, ERC721URIStorage, Ownable {
+contract FractionalLayerNFT is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -22,12 +22,8 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
         uint256 metadataId;
         string attestationId;
         bool exists;
-    }
-
-    struct ShareListing {
+        uint256 price;  // Price in ETH
         uint256 sharesAvailable;
-        uint256 pricePerShare;  // Price in ETH
-        bool isListed;
     }
 
     struct LayerTransferParams {
@@ -40,42 +36,17 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
         uint256 newLayer;
     }
 
-    struct OwnerDetails {
-        address ownerAddress;
-        uint256 shares;
-        ShareListing listing;
-    }
-
     // Mappings
     mapping(uint256 => NFTData) public nftData;
     mapping(address => mapping(uint256 => uint256)) public shareBalances;
-    mapping(uint256 => mapping(address => ShareListing)) public shareListing;
-    mapping(uint256 => address[]) private nftOwners;
-    mapping(uint256 => mapping(address => bool)) private isOwner;
 
     // Events
     event NFTMinted(
         uint256 indexed tokenId,
         address indexed owner,
         uint256 metadataId,
-        string attestationId
-    );
-    event SharesListed(
-        uint256 indexed tokenId,
-        address indexed seller,
-        uint256 shares,
-        uint256 pricePerShare
-    );
-    event SharesUnlisted(
-        uint256 indexed tokenId,
-        address indexed seller
-    );
-    event SharesPurchased(
-        uint256 indexed tokenId,
-        address indexed buyer,
-        address indexed seller,
-        uint256 shares,
-        uint256 pricePerShare
+        string attestationId,
+        uint256 price
     );
     event SharesBurned(
         uint256 indexed tokenId,
@@ -89,7 +60,7 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
         uint256 newLayer
     );
 
-    constructor() ERC721("NFT Shares", "NFTS") Ownable(msg.sender) {}
+    constructor() ERC721("Fractional Layer NFT", "FLNFT") Ownable(msg.sender) {}
 
     // Modifiers
     modifier nftExists(uint256 tokenId) {
@@ -109,13 +80,8 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
         require(params.nftId1 != params.nftId2, "Cannot combine the same NFT");
         require(params.shares1 > 0 && params.shares2 > 0, "Shares must be greater than 0");
         require(
-            shareListing[params.nftId1][params.seller1].isListed &&
-            shareListing[params.nftId2][params.seller2].isListed,
-            "Shares not listed for sale"
-        );
-        require(
-            shareListing[params.nftId1][params.seller1].sharesAvailable >= params.shares1 &&
-            shareListing[params.nftId2][params.seller2].sharesAvailable >= params.shares2,
+            shareBalances[params.seller1][params.nftId1] >= params.shares1 &&
+            shareBalances[params.seller2][params.nftId2] >= params.shares2,
             "Not enough shares available"
         );
         _;
@@ -123,8 +89,11 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
 
     function mintNFT(
         string memory uri,
-        string memory attestationId
+        string memory attestationId,
+        uint256 priceInEth
     ) public returns (uint256) {
+        require(priceInEth > 0, "Price must be greater than 0");
+        
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
 
@@ -138,89 +107,22 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
             layer: INITIAL_LAYER,
             metadataId: metadataId,
             attestationId: attestationId,
-            exists: true
+            exists: true,
+            price: priceInEth,
+            sharesAvailable: INITIAL_SHARES
         });
 
+        // Assign all initial shares to the minter
         shareBalances[msg.sender][tokenId] = INITIAL_SHARES;
 
-        nftOwners[tokenId].push(msg.sender);
-        isOwner[tokenId][msg.sender] = true;
-
-        emit NFTMinted(tokenId, msg.sender, metadataId, attestationId);
+        emit NFTMinted(tokenId, msg.sender, metadataId, attestationId, priceInEth);
         return tokenId;
-    }
-
-    function listShares(
-        uint256 tokenId,
-        uint256 shares,
-        uint256 pricePerShareInEth
-    ) public nftExists(tokenId) hasEnoughShares(msg.sender, tokenId, shares) {
-        require(shares > 0, "Must list at least one share");
-        require(pricePerShareInEth > 0, "Price must be greater than 0");
-
-        shareListing[tokenId][msg.sender] = ShareListing({
-            sharesAvailable: shares,
-            pricePerShare: pricePerShareInEth,
-            isListed: true
-        });
-
-        emit SharesListed(tokenId, msg.sender, shares, pricePerShareInEth);
-    }
-
-    function unlistShares(uint256 tokenId) public {
-        require(shareListing[tokenId][msg.sender].isListed, "No active listing");
-        delete shareListing[tokenId][msg.sender];
-        emit SharesUnlisted(tokenId, msg.sender);
-    }
-
-    function purchaseSharesFromListing(
-        uint256 tokenId,
-        address seller,
-        uint256 shares
-    ) public payable nftExists(tokenId) {
-        ShareListing storage listing = shareListing[tokenId][seller];
-        require(listing.isListed, "Shares not listed for sale");
-        require(listing.sharesAvailable >= shares, "Not enough shares available");
-        require(shares > 0, "Must purchase at least one share");
-
-        uint256 totalCostInWei = listing.pricePerShare * 1 ether * shares;
-        require(msg.value >= totalCostInWei, "Insufficient payment");
-
-        // Transfer shares
-        shareBalances[seller][tokenId] -= shares;
-        shareBalances[msg.sender][tokenId] += shares;
-        
-        // Update listing
-        listing.sharesAvailable -= shares;
-        if (listing.sharesAvailable == 0) {
-            delete shareListing[tokenId][seller];
-        }
-
-        // Transfer payment to seller
-        payable(seller).transfer(totalCostInWei);
-
-        // Refund excess payment
-        if (msg.value > totalCostInWei) {
-            payable(msg.sender).transfer(msg.value - totalCostInWei);
-        }
-
-        // Add new owner to the list if not already present
-        if (!isOwner[tokenId][msg.sender]) {
-            nftOwners[tokenId].push(msg.sender);
-            isOwner[tokenId][msg.sender] = true;
-        }
-
-        // Remove seller from owners list if they sold all shares
-        if (shareBalances[seller][tokenId] == 0) {
-            _removeOwner(tokenId, seller);
-        }
-
-        emit SharesPurchased(tokenId, msg.sender, seller, shares, listing.pricePerShare);
     }
 
     function layerTransfer(
         LayerTransferParams calldata params,
-        string memory uri
+        string memory uri,
+        uint256 newPriceInEth
     ) 
         public 
         payable 
@@ -228,6 +130,8 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
         nftExists(params.nftId2)
         validateLayerTransfer(params)
     {
+        require(newPriceInEth > 0, "New NFT price must be greater than 0");
+        
         // Calculate total cost
         uint256 totalCost = _calculateLayerTransferCost(params);
         require(msg.value >= totalCost, "Insufficient payment");
@@ -236,7 +140,7 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
         _processLayerTransferPayments(params);
 
         // Create new NFT at higher layer
-        uint256 newTokenId = _createLayerNFT(params, uri);
+        uint256 newTokenId = _createLayerNFT(params, uri, newPriceInEth);
 
         // Refund excess payment
         if (msg.value > totalCost) {
@@ -247,33 +151,23 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
     }
 
     function _calculateLayerTransferCost(LayerTransferParams memory params) private view returns (uint256) {
-        uint256 cost1 = shareListing[params.nftId1][params.seller1].pricePerShare * 1 ether * params.shares1;
-        uint256 cost2 = shareListing[params.nftId2][params.seller2].pricePerShare * 1 ether * params.shares2;
+        uint256 cost1 = nftData[params.nftId1].price * 1 ether;
+        uint256 cost2 = nftData[params.nftId2].price * 1 ether;
         return cost1 + cost2;
     }
 
     function _processLayerTransferPayments(LayerTransferParams memory params) private {
-        ShareListing storage listing1 = shareListing[params.nftId1][params.seller1];
-        ShareListing storage listing2 = shareListing[params.nftId2][params.seller2];
-
         // Calculate payments
-        uint256 payment1 = listing1.pricePerShare * 1 ether * params.shares1;
-        uint256 payment2 = listing2.pricePerShare * 1 ether * params.shares2;
+        uint256 payment1 = nftData[params.nftId1].price * 1 ether;
+        uint256 payment2 = nftData[params.nftId2].price * 1 ether;
 
-        // Update share balances and burn shares
+        // Burn shares
         shareBalances[params.seller1][params.nftId1] -= params.shares1;
         shareBalances[params.seller2][params.nftId2] -= params.shares2;
 
-        // Update listings
-        listing1.sharesAvailable -= params.shares1;
-        listing2.sharesAvailable -= params.shares2;
-
-        if (listing1.sharesAvailable == 0) {
-            delete shareListing[params.nftId1][params.seller1];
-        }
-        if (listing2.sharesAvailable == 0) {
-            delete shareListing[params.nftId2][params.seller2];
-        }
+        // Update NFT data
+        nftData[params.nftId1].sharesAvailable -= params.shares1;
+        nftData[params.nftId2].sharesAvailable -= params.shares2;
 
         // Transfer payments to sellers
         payable(params.seller1).transfer(payment1);
@@ -286,7 +180,8 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
 
     function _createLayerNFT(
         LayerTransferParams memory params,
-        string memory uri
+        string memory uri,
+        uint256 newPriceInEth
     ) private returns (uint256) {
         uint256 newTokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
@@ -301,7 +196,9 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
             layer: params.newLayer,
             metadataId: newMetadataId,
             attestationId: "",
-            exists: true
+            exists: true,
+            price: newPriceInEth,
+            sharesAvailable: INITIAL_SHARES
         });
 
         // Assign initial shares to minter
@@ -324,13 +221,6 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
         return shareBalances[owner][tokenId];
     }
 
-    function getShareListing(
-        uint256 tokenId,
-        address seller
-    ) public view returns (ShareListing memory) {
-        return shareListing[tokenId][seller];
-    }
-
     // Override required functions
     function tokenURI(
         uint256 tokenId
@@ -342,41 +232,5 @@ contract NFTShares is ERC721, ERC721URIStorage, Ownable {
         bytes4 interfaceId
     ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
-    }
-
-    // Add this function to get all owners and their details
-    function getNFTOwners(uint256 tokenId) 
-        public 
-        view 
-        nftExists(tokenId) 
-        returns (OwnerDetails[] memory) 
-    {
-        address[] memory owners = nftOwners[tokenId];
-        OwnerDetails[] memory details = new OwnerDetails[](owners.length);
-        
-        for (uint i = 0; i < owners.length; i++) {
-            address owner = owners[i];
-            details[i] = OwnerDetails({
-                ownerAddress: owner,
-                shares: shareBalances[owner][tokenId],
-                listing: shareListing[tokenId][owner]
-            });
-        }
-        
-        return details;
-    }
-
-    // Add helper function to remove owner
-    function _removeOwner(uint256 tokenId, address owner) private {
-        for (uint i = 0; i < nftOwners[tokenId].length; i++) {
-            if (nftOwners[tokenId][i] == owner) {
-                // Move the last element to the position being deleted
-                nftOwners[tokenId][i] = nftOwners[tokenId][nftOwners[tokenId].length - 1];
-                // Remove the last element
-                nftOwners[tokenId].pop();
-                isOwner[tokenId][owner] = false;
-                break;
-            }
-        }
     }
 }
